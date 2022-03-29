c4082958380ff9105d0c1a308e86350bb1ef6a41
ubelaggin <48964563+ubelaggin@users.noreply.github.com>
2022-03-29 14:35:26 +0300

    add

--- JavaScriptCore/in-class-tasks/npmModules/Task 2/Template/logger.js
+++ JavaScriptCore/in-class-tasks/npmModules/Task 2/Template/logger.js
@@ -1,11 +1,22 @@
 const defaultInfoMsg = 'No info provided!';
 
 const info = (msg = defaultInfoMsg) => {
-console.log(msg)
+console.log(`Message: ${msg||defaultInfoMsg}`)
 };
 
+const defaultError = 'No error provided!'
+
+const error = (msg = defaultError)=>{
+  console.log(`Error: ${msg || defaultError}`)
+}
+
+const defaultSuccess = 'No success provided!'
+
+const success = (msg = defaultSuccess)=>{
+  console.log(`Success: ${msg||defaultSuccess}`)
+}
 export default {
   info,
-  // error,
-  // success,
+  error,
+  success,
 };
--- JavaScriptCore/in-class-tasks/npmModules/Task 2/Template/main.js
+++ JavaScriptCore/in-class-tasks/npmModules/Task 2/Template/main.js
@@ -1,7 +1,10 @@
-// import moment module
-// import logger module
-import log from './logger'
-log.info()
+import moment from 'moment';
+moment().format();
+
+import log from './logger.js'
+log.info(moment.utc())
+log.error()
+log.success()
 // do not remove this line. Still, if you want google it :)
 moment.suppressDeprecationWarnings = true;
 
@@ -9,3 +12,11 @@ moment.suppressDeprecationWarnings = true;
 
 const dates = ["1995-12-25", "2020W065", "js-cohort", "2020-W06-5"];
 // Parse the array and logs success when date is valid or error when invalid using the logger
+
+dates.forEach(date => {
+    if (moment(date).isValid()) {
+      log.success(moment(date));
+    } else {
+      log.error(moment(date));
+    }
+  });
